46641e8db6de9c0b92259e6d7dbe68c31eda64ba
Eremuko X <jpmelizalde@gmail.com>
2023-07-12 09:43:39 -0500

    hw06

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,51 @@
 (() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+  
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+  
+  var input = document.getElementById('user-input');
+  
+  input.addEventListener('focus', function() {
+    var personaIconElement = document.querySelector('.persona');
+    personaIconElement.style.fill = '#2196F3';
+  });
+  
+  input.addEventListener('blur', function() {
+    var personaIconElement = document.querySelector('.persona');
+    personaIconElement.style.fill = '#757575';
+  });
 
-const refs = {
-openModalBtn: document.querySelector("[data-modal-open]"),
-closeModalBtn: document.querySelector("[data-modal-close]"),
-modal: document.querySelector("[data-modal]"),
-};
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
-function toggleModal() {
-refs.modal.classList.toggle("is-hidden");
-}
+   var input = document.getElementById('user-telefono');
+  
+  input.addEventListener('focus', function() {
+    var personaIconElement = document.querySelector('.telefono');
+    personaIconElement.style.fill = '#2196F3';
+  });
+  
+  input.addEventListener('blur', function() {
+    var personaIconElement = document.querySelector('.telefono');
+    personaIconElement.style.fill = '#757575';
+  });
+
+  var input = document.getElementById('user-email');
+  
+  input.addEventListener('focus', function() {
+    var personaIconElement = document.querySelector('.email');
+    personaIconElement.style.fill = '#2196F3';
+  });
+  
+  input.addEventListener('blur', function() {
+    var personaIconElement = document.querySelector('.email');
+    personaIconElement.style.fill = '#757575';
+  });
+   
 })();
